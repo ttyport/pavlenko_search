3457ba8cdca01004ea74bad427c1562e5c6cfea5
Nikolay Chechulin <nchechulin@protonmail.com>
2020-10-21 18:57:40 +0300

    add docstrings and comments to config.py

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
+'''
+Constants for configuring the bot
+'''
+
+
+# Bot configuration
 API_KEY = ''
 BOT_TOKEN = ''
 BOT_OWNERS = ['473513901', '391043684']
 CHANNEL_ID = 'UC_hvS-IJ_SY04Op14v3l4Lg'
 
+# Standart messages
 WELCOME_MESSAGE = 'Привет, напиши мне пару слов из названия видео, а я отправлю тебе ссылку на него.'
 HELP_MESSAGE = '''
 Привет, напиши мне пару слов из названия видео, а я отправлю тебе ссылку на него.
